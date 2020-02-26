f80e3d1e09810c682fd995a8e47e75b76a9a1fb2
unknown <elinescu.alexandru@gmail.com>
2020-02-26 20:52:46 +0200

    Commit feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel develop
+                    Laravel develop feature
                 </div>
 
                 <div class="links">
