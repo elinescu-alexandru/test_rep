17eaf0664ed18d66030a9059a46fa49bc0719644
unknown <elinescu.alexandru@gmail.com>
2020-02-26 20:40:23 +0200

    Commit develop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel develop
                 </div>
 
                 <div class="links">
