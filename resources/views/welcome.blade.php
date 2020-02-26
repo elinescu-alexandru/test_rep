5843ba1cba2655797656dc9f478b82061f2ddaaf
unknown <elinescu.alexandru@gmail.com>
2020-02-26 21:02:19 +0200

    Commit feature 1.0 h1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Laravel develop feature 1.1
                 </div>
-
+                <h1>Din feature</h1>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
